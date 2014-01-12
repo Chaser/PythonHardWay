8859befdb68ceb097c05e2be68b02574d86a099d
cjohnson <chaseajohnson@gmail.com>
2014-01-12 20:44:53 +1000

    Ex 03 Completion

---
+++ Ex03/Ex03_Maths.py
@@ -0,0 +1,24 @@
+print "I will now count my chickens:"
+
+print "Hens", 25+30/6						#30/6 then adds 25 = 30
+print "Roosters", 100-25*3%4				#4 into 75 is 3. Taken away from 100 is 97
+
+print "Now I will count the eggs:"
+
+print 3+2+1-5+4%2-1/4+6					 	#2 into 4 goes twice no remainder.  
+
+print "Is it true that 3+2<5-7"
+
+print 3+2<5-7								
+
+print "What is 3+2?", 3+2
+print "what is 5-7",5-7
+
+print "Oh that's why it is False."
+
+print "How about some more."
+
+print "Is it greater?",5>-2
+print "Is it greater or equal?",5>=-2
+print "Is it less or equal", 5<=-2
+
---
+++ Ex03/Ex03_Maths_StudeyDrill5.py
@@ -0,0 +1,24 @@
+print "I will now count my chickens:"
+
+print "Hens", 25.0+30.0/6.0-2										#30/6 then adds 25 = 30
+print "Roosters", 100.0-25.0*3.0%4.0								#4 into 75 is 3. Taken away from 100 is 97
+
+print "Now I will count the eggs:"
+
+print "%0.2f" % (3.0+2+1-5.0+4.0%2.0-2.0/4.0+6.0)					#2 into 4 goes twice no remainder.  
+
+print "Is it true that 3+2<5-7"
+
+print 3+2<5-7								#False
+
+print "What is 3+2?", 3+2 					#5
+print "what is 5-7",5-7						#-2
+
+print "Oh that's why it is False."
+
+print "How about some more."
+
+print "Is it greater?",5>-2    				#True
+print "Is it greater or equal?",5>=-2    	#True
+print "Is it less or equal", 5<=-2 			#False
+
