62fec8ca89ebb7bc54059d634aa384376eeb17f7
cjohnson <chaseajohnson@gmail.com>
2014-01-13 19:03:07 +1000

    Ex10 (Special Character) completion

---
+++ Ex10/Ex10_WhatWasThat.py
@@ -0,0 +1,38 @@
+
+# ~~~~~~~~ ESCAPE Characters
+# 		\\			Backslash
+#		\'			Single Quote
+#		\"			Doub;le Quote
+# 		\a			ASCII Bell (BEL)
+#		\b 			ASCII Backspace (BS)
+#		\f  		ASCII formfeed (FF)
+#		\n 			ASCII linefeed (LF)
+#		\N{name}	Character named name in the unicode database (Unicode only)
+# 		\r 			ASCII Carriage Return (CR)
+#		\t 			ASCII Horizontal Tabe (TAB)
+# 		\uxxxx		Character with 16-bit hex value xxxx (Unicode only)
+#		\Uxxxxxxxx	Character with 32-bit hex value xxxxxxxx (Unicode only)
+#		\v  		ASCII Vertical Tabe (VT)
+#		\ooo		Character with octal value ooo
+#	  	\xhh		Character with hex value hh
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat ="I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies 
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+
+#while True:
+#	for i in ["/","-","|","\\","|"]:
+#		print "%s\r" %i
