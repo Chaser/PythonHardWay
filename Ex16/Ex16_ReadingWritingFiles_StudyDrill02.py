08551c8df8845584f194c03c0063b90da73ba2e6
cjohnson <chaseajohnson@gmail.com>
2014-01-16 18:08:18 +1000

    Ex16 (Reading/Writing Files) completion

---
+++ Ex16/Ex16_ReadingWritingFiles_StudyDrill02.py
@@ -0,0 +1,42 @@
+#import of modules/libraries
+from sys import argv
+
+#argv is "argument variable". Holds the arguments you pass to your Python script
+#when you run it
+script, filename = argv
+#Command line arguments are always strings!!!
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit RETURN"
+
+raw_input("?")
+
+print "Opening the file..."
+target = open(filename,'w')
+
+print "Trucating the file. Goodbye!"
+target.truncate()			#Empties the file.
+
+print "Now I'm going to ask you for three lines."
+
+line1 = raw_input("line 1: ")
+line2 = raw_input("line 2: ")
+line3 = raw_input("line 3: ")
+
+print "I'm going to write these to the file"
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+
+print "And finally, we close it"
+target.close()
+
+print "Checking file write..."
+readfile = open(filename,'r')
+
+print 'Displaying File Contents'
+print readfile.read();
