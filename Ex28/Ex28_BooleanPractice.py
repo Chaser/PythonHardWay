d0fcad7fa07f49501a4d5a372a989dc94dec7767
cjohnson <chaseajohnson@gmail.com>
2014-02-07 19:17:12 +1000

    Ex28 completion (Boolean logic)

---
+++ Ex28/Ex28_BooleanPractice.py
@@ -0,0 +1,24 @@
+ #NOT Routines
+print "Boolean Practice\n"
+print "1. True and True : %s\n" % (True and True)					
+print "2. False and True : %s\n" % (False and True)					
+print "3. 1 == 1 and 2==1 : %s\n" % (1==1 and 2==1)					
+print "4. 'Test' == 'Test' : %s\n" % ("Test" == "Test")
+print "5. 1 == 1 or '2 != 1 : %s\n" % (1 == 1 or 2 != 1)
+print "6. True and 1 == 1 : %s\n" % (True and 1 == 1)
+print "7. False and 0 != 0 : %s\n" % (False and 0 != 0)
+print "8. True or 1 == 1 : %s\n" % (True or 1 == 1)
+print "9. 'Test' == 'Testing' : %s\n" % ('Test' == 'Testing')
+print "10. 1 != 0 and 2 == 1 : %s\n" % (1 != 0 and 2 ==1)
+print "11. 'test' !=  'testing' : %s\n" % ('test' != 'testing')
+print "12.'test' == 1 : %s\n" % ('test' == 1)
+print "13. not(True and False): %s\n" % (not(True and False))
+print "14. not(1 == 1 and 0 != 1): %s\n" % (not(1 == 1 and 0 != 1))
+print "15. not(10 == 1 or 1000 == 1000): %s\n" % (not(10 == 1 or 1000 == 1000))
+print "16. not(1 ! = 10 or 3 == 4): %s\n" % (not(1 != 10 or 3 == 4))
+print "17. not('testing' == 'testing' and 'Zed'  == 'Cool Guy': %s\n" % (not('testing' == 'testing' and 'Zed'  == 'Cool Guy'))
+print "18. 1 == 1 and not('testing' == 1 or 1 == 0): %s\n" % (1 == 1 and not('testing' == 1 or 1 == 0))
+print "19. 'chunky' == 'bacon' and not (3 == 4 or 3 == 3): %s\n" % ('chunky' == 'bacon' and not (3 == 4 or 3 == 3))
+print "20. 3 == 3 and not ('testing' == 'testing' or 'Python' == 'Fun'): %s\n" % (3 == 3 and not ('testing' == 'testing' or 'Python' == 'Fun'))
+
+
