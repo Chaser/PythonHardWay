73022c3157c340cd024751742ad44de3cb97a579
cjohnson <chaseajohnson@gmail.com>
2014-03-19 08:53:17 +1000

    Ex44 (Inheritance vs composition)

---
+++ Ex44/Ex44_InheritanceVsComposition.py
@@ -0,0 +1,99 @@
+
+
+#Implicit Inheritance 
+#Implicit actions that happen when you defina a function in a parent but not in the child
+
+class Parent(object):
+
+	def implicit(self):
+		print "PARENT implicit()"
+
+
+class Child(Parent):
+	#pass tells Python you want an empty block
+	pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+
+
+#Override Explicity
+class Parent(object):
+	
+	def override(self):
+		print "PARENT override()"
+
+class Child(Parent):
+
+	def override(self):
+		print "CHILD override()"
+
+
+dad = Parent()
+son = Child()
+
+dad.override()
+son.override()
+
+#Alter before or After
+class Parent(object):
+
+	def altered(self):
+		print "PARENT altered()"
+
+class Child(Parent):
+
+	def altered(self):
+		print "CHILD, BEFORE PARENT altered()"
+		#Call super with arguments Child and self, then call the function altered on whatever it returns".
+		super(Child,self).altered()
+		print "CHILD, AFTER PARENT altered()"
+
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
+
+
+
+#Composition 
+class Other(object):
+
+	def override(self):
+		print "OTHER override()"
+
+	def implicit(self):
+		print "OTHER implicit()"
+
+	def altered(self):
+		print "OTHER altered()"
+
+class Child(object):
+
+	def __init__(self):
+		self.other = Other()
+
+	def implicit(self):
+		self.other.implicit()
+
+	def override(self):
+		print "CHILD override"
+
+	def altered(self):
+		print "CHILD, BEFORE OTHER altered()"
+		self.other.altered()
+		print "CHILD, AFTER OTHER altered()"
+
+son = Child()
+son.implicit()
+son.override()
+son.altered()
+
+
+
+
